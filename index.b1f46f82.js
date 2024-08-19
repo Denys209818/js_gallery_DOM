9b9d72dc20734eb3c6e06c6ad1aa9025798f6084
Denys209818 <deniskravcuk63@gmail.com>
2024-08-19 20:19:39 +0300

    make build

---
+++ index.b1f46f82.js
@@ -0,0 +1,2 @@
+var e=document.querySelector(".gallery__large-img");document.querySelector(".gallery__list").addEventListener("click",function(t){t.preventDefault(),t.stopPropagation();var r=t.target.closest(".list-item__link");e.src=r.href});
+//# sourceMappingURL=index.b1f46f82.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,11 @@
+"use strict";
+const mainImage = document.querySelector(".gallery__large-img");
+const list = document.querySelector(".gallery__list");
+list.addEventListener("click", (e)=>{
+    e.preventDefault();
+    e.stopPropagation();
+    const current = e.target.closest(".list-item__link");
+    mainImage.src = current.href;
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
